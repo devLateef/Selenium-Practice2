869db193a9aa0f586d9fe2500d1281039913583b
devLateef <lateefolatayo@gmail.com>
2024-01-09 15:44:22 +0100

    Added 2 methods to Animal Class

--- src/test/java/OPPConcept/Animal.java
+++ src/test/java/OPPConcept/Animal.java
@@ -2,7 +2,16 @@ package OPPConcept;
 
 public class Animal {
 
-    public static void walking(String name) {
-        System.out.println(name + " is walking");
+    public static String walking(String name) {
+//        System.out.println(name + " is walking");
+        return name + " is walking";
+    }
+
+    public static String eating(String message){
+        return message;
+    }
+
+    public static String blinking(String message){
+        return message;
     }
 }
