0a94f3753194232b43b6298176283fa4ce1f8afa
devLateef <lateefolatayo@gmail.com>
2024-01-09 17:52:19 +0100

    Edited a file

--- src/test/java/OPPConcept/Cat.java
+++ src/test/java/OPPConcept/Cat.java
@@ -3,7 +3,7 @@ package OPPConcept;
 public class Cat extends Animal{
     public static void main(String[] args) {
         System.out.println(walking("Bingo"));
-        System.out.println(eating("I can eat very very well"));
+        System.out.println(eating("I can eat very well"));
         System.out.println(blinking("I am blinking"));
     };
 
